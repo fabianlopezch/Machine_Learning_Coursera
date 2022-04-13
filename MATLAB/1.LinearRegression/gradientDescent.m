2570a094ffc578c1b096d15eb3e202ce67270a2c
Fabian Lopez Chamorro <fabianlopezch@outlook.com>
2022-04-13 16:59:39 -0500

    Gradient descent implementation

--- MATLAB/1.LinearRegression/gradientDescent.m
+++ MATLAB/1.LinearRegression/gradientDescent.m
@@ -18,9 +18,8 @@ for iter = 1:num_iters
     %
 
 
-
-
-
+    Delta = ((theta' * (X' * X)) - (y' * X))';
+    theta = theta - (alpha/m) .* Delta;
 
 
     % ============================================================
