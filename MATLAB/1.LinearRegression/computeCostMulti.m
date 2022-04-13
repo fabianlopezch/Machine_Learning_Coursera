17b12146a017065f1f40882dc65bbf96671cc268
Fabian Lopez Chamorro <fabianlopezch@outlook.com>
2022-04-13 17:55:10 -0500

    Implementing Multivariate Linear Regression

--- MATLAB/1.LinearRegression/computeCostMulti.m
+++ MATLAB/1.LinearRegression/computeCostMulti.m
@@ -13,8 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+A = X*theta - y;
 
-
+J = A' * A;
+J = J / (2*m);
 
 
 % =========================================================================
--- MATLAB/1.LinearRegression/gradientDescentMulti.m
+++ MATLAB/1.LinearRegression/gradientDescentMulti.m
@@ -19,10 +19,8 @@ for iter = 1:num_iters
 
 
 
-
-
-
-
+    Delta = ((theta' * (X' * X)) - (y' * X))';
+    theta = theta - (alpha/m) .* Delta;
 
 
 
