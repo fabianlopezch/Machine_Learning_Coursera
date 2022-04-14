b12cf6281fbccc9c10d8d9bc5f1b5e08b66b2ec5
Fabian Lopez Chamorro <fabianlopezch@outlook.com>
2022-04-13 23:30:16 -0500

    Implementing the plotData function for data visualization

--- MATLAB/2.LogisticRegression/plotData.m
+++ MATLAB/2.LogisticRegression/plotData.m
@@ -14,9 +14,14 @@ figure; hold on;
 
 
 
+% gscatter(X(:,1),X(:,2),y,'kk','o+', [4 4]) % Alternative implementation
+% with gscatter
 
-
-
+% Find Indices of Positive and Negative Examples
+pos = find(y==1); neg = find(y == 0);
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y','MarkerSize', 7);
 
 
 
