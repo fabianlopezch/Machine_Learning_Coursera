e3dbb6890ee04264dc30f01b91133aec66b20b81
Fabian Lopez Chamorro <fabianlopezch@outlook.com>
2022-04-20 19:59:54 -0500

    Implementing One-vs-All classification

--- MATLAB/3.NeuralNetworks_Representation/oneVsAll.m
+++ MATLAB/3.NeuralNetworks_Representation/oneVsAll.m
@@ -8,8 +8,8 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %   to the classifier for label i
 
 % Some useful variables
-m = size(X, 1);
-n = size(X, 2);
+m = size(X, 1); % Numner of data examples
+n = size(X, 2); % Number of features
 
 % You need to return the following variables correctly 
 all_theta = zeros(num_labels, n + 1);
@@ -49,7 +49,20 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
+for i=1:num_labels
+    
+    initial_theta = zeros(n+1,1);
+
+    [theta] = ...
+         fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), ...
+                initial_theta, options);
+
+    all_theta(i,:) = theta;
+
+end
 
 
 
